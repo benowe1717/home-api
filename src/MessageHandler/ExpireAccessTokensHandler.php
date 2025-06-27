560ecb90e4b4b011f9f24644867b6fe15ed16af5
Benjamin Owen <benjamin@projecttiy.com>
2025-06-27 16:54:55 -0400

    created ExpireAccessTokens Message and ExpireAccessTokensHandler Message Handler class to handle those messages, set up a recurring schedule in ExpireAccessTokensProvider to run the expiry logic

---
+++ src/MessageHandler/ExpireAccessTokensHandler.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Symfony Message Handler for ExpireAccessTokens Message
+ *
+ * PHP version 8.4
+ *
+ * @category  MessageHandler
+ * @package   Home-API
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2025 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   CVS: $Id:$
+ * @link      https://github.com/benowe1717/home-api
+ **/
+
+namespace App\MessageHandler;
+
+use App\Entity\AccessToken;
+use App\Message\ExpireAccessTokens;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+/**
+ * Symfony Message Handler for ExpireAccessTokens Message
+ *
+ * PHP version 8.4
+ *
+ * @category  MessageHandler
+ * @package   Home-API
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   Release: 0.0.1
+ * @link      https://github.com/benowe1717/home-api
+ **/
+#[AsMessageHandler]
+final class ExpireAccessTokensHandler
+{
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * ExpireAccessTokensHandler constructor
+     *
+     * @param EntityManagerInterface $entityManager The Entity Manager
+     **/
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Return all Access Token objects found in the database
+     *
+     * @return array
+     **/
+    private function getAccessTokens(): array
+    {
+        $repo = $this->entityManager->getRepository(AccessToken::class);
+        return $repo->findAll();
+    }
+
+    /**
+     * Take action on all emitted ExpireAccessTokens messages
+     *
+     * @param ExpireAccessTokens $message The ExpireAccessTokens message
+     *
+     * @return void
+     **/
+    public function __invoke(ExpireAccessTokens $message): void
+    {
+        $now = $message->getNow();
+        $accessTokens = $this->getAccessTokens();
+
+        /**
+         * The Access Token Entity to operate on
+         *
+         * @var AccessToken $accessToken
+         **/
+        foreach ($accessTokens as $accessToken) {
+            $expiresAt = $accessToken->getExpires();
+            if ($now >= $expiresAt) {
+                $this->entityManager->remove($accessToken);
+                $this->entityManager->flush();
+            }
+        }
+    }
+}
---
+++ src/Scheduler/ExpireAccessTokensProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Symfony Scheduler for Expiring Access Tokens
+ *
+ * PHP version 8.4
+ *
+ * @category  Scheduler
+ * @package   Home-API
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2025 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   CVS: $Id:$
+ * @link      https://github.com/benowe1717/home-api
+ **/
+
+namespace App\Scheduler;
+
+use App\Message\ExpireAccessTokens;
+use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\RecurringMessage;
+use Symfony\Component\Scheduler\Schedule;
+use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+/**
+ * Symfony Scheduler for Expiring Access Tokens
+ *
+ * PHP version 8.4
+ *
+ * @category  Scheduler
+ * @package   Home-API
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2025 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   Release: 0.0.1
+ * @link      https://github.com/benowe1717/home-api
+ **/
+#[AsSchedule('expire-accesstokens')]
+final class ExpireAccessTokensProvider implements ScheduleProviderInterface
+{
+    /**
+     * Get the recurring schedule to emit the desired Message to be handled by
+     * the configured Message Handler
+     *
+     * @return Schedule
+     **/
+    public function getSchedule(): Schedule
+    {
+        return new Schedule()
+            ->add(
+                RecurringMessage::every('4 hours', new ExpireAccessTokens())
+            );
+    }
+}
